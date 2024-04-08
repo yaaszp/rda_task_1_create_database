c67cb71cb398ae42a1ff6eb3c493a8e733095a64
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-04-08 18:32:53 +0300

    create database

--- task.sql
+++ task.sql
@@ -1,1 +1,26 @@
 # Write your SQL code for the database creation here. Good luck! 
+
+CREATE DATABASE ShopDB;
+
+USE ShopDB;
+
+CREATE TABLE Products (
+    ID INT AUTO_INCREMENT,
+    ProductName VARCHAR(50),
+    ProductDescription VARCHAR(100),
+    Price INT,
+    WarehouseAmount INT,
+    HireDate DATE,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Customers (
+    ID INT AUTO_INCREMENT,
+    ProductName VARCHAR(50),
+    ProductDescription VARCHAR(100),
+    Price INT,
+    WarehouseAmount INT,
+    HireDate DATE,
+    PRIMARY KEY (ID)
+);
+
