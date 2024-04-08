ba69d53a8ff65517c1e2a35540596b47fbec245e
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-04-08 19:11:41 +0300

    create tables

--- task.sql
+++ task.sql
@@ -16,11 +16,26 @@ CREATE TABLE Products (
 
 CREATE TABLE Customers (
     ID INT AUTO_INCREMENT,
-    ProductName VARCHAR(50),
-    ProductDescription VARCHAR(100),
-    Price INT,
-    WarehouseAmount INT,
-    HireDate DATE,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(50),
+    CustomerAddress VARCHAR(100),
     PRIMARY KEY (ID)
 );
 
+CREATE TABLE Orders (
+    ID INT AUTO_INCREMENT,
+    CustomerID INT,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE CASCADE,
+    OrderDate DATE,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE OrderItems (
+    ID INT AUTO_INCREMENT,
+    OrderID INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE CASCADE,
+    ProductID INT,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE CASCADE,
+    PRIMARY KEY (ID)
+);
